71a466a7b215e2da27335b3205f129616d9200aa
Peter J Smith <pjstarifa@gmail.com>
2012-09-01 16:48:11 +0100

    Added toastr

--- js/app.js
+++ js/app.js
@@ -14,7 +14,6 @@ function FiddleCtrl($scope, $http){
         var height= $(window).height()-80;
         $('#fiddle').css('height',height +'px');
         $('#intro').hide();
-
         setTimeout(function(){
             $('#fiddle').attr('src',url);
         },100);
@@ -23,17 +22,27 @@ function FiddleCtrl($scope, $http){
         console.log('Fiddle loaded');
     };
 
-    $scope.getFiddles=function(){
-        $http({method: 'JSONP', url: 'http://jsfiddle.net/api/user/angularplayground/demo/list.json?callback=JSON_CALLBACK'})
+
+
+    $scope.getFiddles=function(userName){
+        $scope.fiddles={};
+        if(userName==null){
+           // toastr.warning('Enter a user name', '');
+            return;
+        };
+        $scope.userName=userName;
+
+        $http({method: 'JSONP', url: 'http://jsfiddle.net/api/user/' + userName + '/demo/list.json?callback=JSON_CALLBACK'})
         .success(function(data, status, headers, config) {
             $scope.fiddles=data;
             console.log($scope.fiddles);
+
         }).
         error(function(data, status, headers, config) {
             $scope.error='Error';
+                if(status=404){toastr.error('User not found', '');} ;
         });
     };
 
-    $scope.getFiddles();
 };
 
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
-Welcome to the AngularJS Playground
+Welcome to the AngularJS Playground.
+
+This app accepts a jsFiddle username and attempts to retrieve a list of fiddles for that user via the  jsFiddle API.
